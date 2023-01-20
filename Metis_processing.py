eec0e543a83f81722402db3b599ecab048149f8d
chronque <csg524@york.ac.uk>
2023-01-20 16:31:52 +0000

    added metis data files

--- Metis_processing.py
+++ Metis_processing.py
@@ -1,10 +1,21 @@
+# This code loads the data from the .mat file and plots a particular variable
+# The default is to plot temperature, but you can change this by changing the
+# index variable
+# The data is from a simulation of a jet of plasma with a magnetic field
+# The variable of interest is the temperature of the plasma
+# The data is saved in a .mat file
+# The .mat file is loaded into a dictionary, and the data for the temperature
+# is extracted
+# The temperature is plotted
+
 #!/usr/bin/env python3
 
 import scipy.io
-import numpy as np
 from matplotlib import pyplot as plt
+import numpy as np
 
-full_dataset = scipy.io.loadmat("./defaultjet_output_save.mat")
+# load the data
+full_dataset = scipy.io.loadmat("defaultjet_output_save.mat")
 
 
 def list_subsections():
@@ -24,6 +35,8 @@ def get_variable(index, subsection='zerod'):
 
 def get_average(start, end, index, subsection='zerod'):
     a = get_variable(index, subsection=subsection)
+    print("DEBUG: getting average for index", index, "from", start, "to", end)
+    #print("DEBUG: raw data is:", a)
     return (np.mean(a[start:end]), np.std(a[start:end]))
 
 
