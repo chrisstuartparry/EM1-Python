8695d83220b07f3a49c0482dd02e9901b8f939b6
chronque <csg524@york.ac.uk>
2023-01-24 12:19:19 +0000

    have it loop over 2-3 files

--- Metis_processing.py
+++ Metis_processing.py
@@ -1,70 +1,37 @@
-#!/usr/bin/env python3
-
 import scipy.io
-from matplotlib import pyplot as plt
 import numpy as np
-import csv
+import matplotlib.pyplot as plt
 
 # load the data
-full_dataset = scipy.io.loadmat(
-    "EM1 Data/Second Run Data (standard mode)/2023-01-20 NBI Power 2MW.mat"
-)
-
-
-def list_subsections():
-    print("subsections:")
-    print(full_dataset["post"].dtype)
-
 
 chosen_subsection = "zerod"
-chosen_index = "te0"
 
 
-def list_indexes(subsection):
-    print("indexes in subsection " + subsection + ":")
-    print(full_dataset["post"][chosen_subsection][0][0].dtype)
+def get_average(file_path, start, end, variables):
+    full_dataset = scipy.io.loadmat(file_path)
+    results = []
+    for variable in variables:
+        a = full_dataset["post"][chosen_subsection][0][0][variable][0][0]
+        a = [float(x[0]) for x in a]
+        avg = np.mean(a[start:end])
+        std = np.std(a[start:end])
+        results.append([variable, avg, std])
+    return results
 
 
-def get_variable(index, subsection):
-    a = full_dataset["post"][subsection][0][0][index][0][0]
-    a = [float(x[0]) for x in a]
-    return a
+files_paths = [
+    "EM1 Data/Second Run Data (standard mode)/2023-01-20 NBI Power 2MW.mat",
+    "EM1 Data/Second Run Data (standard mode)/2023-01-20 NBI Power 4MW.mat",
+    "EM1 Data/Second Run Data (standard mode)/2023-01-20 NBI Power 4MW.mat"
+]
 
+variables = ["te0", "ne0", "taue"]
+start = 50
+end = 100
 
-def get_average(start, end, index, subsection):
-    a = get_variable(index, subsection=subsection)
-    print("DEBUG: getting average for variable", index, "from", start, "to", end)
-    # print("DEBUG: raw data is:", a)
-    return (np.mean(a[start:end]), np.std(a[start:end]), index)
-
-
-# list_subsections()
-# list_indexes()
-# print(get_average(50, 100, 'te0'))
-# plt.plot(get_variable('te0'))
-# plt.show()
-
-print(
-    "Average for variable ",
-    get_average(50, 100, chosen_index, chosen_subsection)[2],
-    " is: ",
-    get_average(50, 100, chosen_index, chosen_subsection)[0],
-    " +/- ",
-    get_average(50, 100, chosen_index, chosen_subsection)[1],
-    " (std)",
-)
-
-result = get_average(50, 100, chosen_index, chosen_subsection)
-
-# open a csv file to write the results
-
-with open("results.csv", mode="w", newline="") as results_file:
-    results_writer = csv.writer(
-        results_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
-    )
-    # write the header row
-    results_writer.writerow(["Variable", "Average", "STD"])
-    # write the results
-    results_writer.writerow([result[2], result[0], result[1]])
-
-print("Results written to results.csv.")
+for file_path in files_paths:
+    results = get_average(file_path, start, end, variables)
+    for variable, avg, std in results:
+        print(
+            f"Average for variable {variable} in {file_path} is: {avg} +/- {std} (std)"
+        )
