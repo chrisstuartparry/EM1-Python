e1470834dbe970e4d164aafd475ae27708cd4ac1
chronque <csg524@york.ac.uk>
2023-02-03 15:54:52 +0000

    created new files for week 3 data analysis

--- week3(stepped_ramp).py
+++ week3(stepped_ramp).py
@@ -12,14 +12,14 @@ save_graph = False
 if save_graph:
     fig_file = input("Enter the name of the file to save the graph to: ")
 
-base_path = "EM1 Data/6th Run Data (fast mode)"
-file_name_template = "2023-01-31 NBI Power 2MW nbar {nbar_value}.mat"
-nbar_values = np.array(range(1,41,1), dtype=float)/10
+base_path = "EM1 Data/8th Run Data (fast mode)"
+file_name_template = "2023-02-03 NBI Ramping 0 to {last_pnbi_value}MW.mat"
+last_pnbi_values = np.array(range(0,11,2), dtype=float)
 
 files_paths = [
-    os.path.join(base_path, file_name_template.format(nbar_value
+    os.path.join(base_path, file_name_template.format(last_pnbi_value
 =value))
-    for value in nbar_values
+    for value in last_pnbi_values
 ]
 
 
@@ -62,21 +62,21 @@ plt.rcParams["text.latex.preamble"] = "\n".join(
     ]
 )
 fig.suptitle(
-    "Plots of the average values of the chosen variables against nbar at an NBI power of 2 MW",
+    "Plots of the average values of the chosen variables against NBI power",
     fontsize=16,
 )
 axs[0].set_title("Triple Product")
-axs[0].set_xlabel("nbar (m$^{-3}$)")
+axs[0].set_xlabel("NBI Power (MW)")
 axs[0].set_ylabel("nTtaue")
-for file_path, power in zip(files_paths, nbar_values):
+for file_path, power in zip(files_paths, last_pnbi_values):
     results = get_triple_product(file_path, start, end)
     triple_product, avg, std = results
     axs[0].errorbar(power, avg, yerr=std, fmt=".", color="black", elinewidth=0.5)
 for i, variable in enumerate(variables):
     axs[i+1].set_title(f"{variable}")
-    axs[i+1].set_xlabel("nbar (m$^{-3}$)")
+    axs[i+1].set_xlabel("NBI Power (MW)")
     axs[i+1].set_ylabel(variable)
-    for file_path, power in zip(files_paths, nbar_values):
+    for file_path, power in zip(files_paths, last_pnbi_values):
         # print(f"Getting data for {variable} at {power} MW")
         results = get_average(file_path, start, end, variables)
         variable, avg, std = results[i]
