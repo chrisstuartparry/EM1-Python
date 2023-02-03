262ca475a75bf92d72a6d1d640d6fee90f557d0c
chronque <csg524@york.ac.uk>
2023-02-03 16:27:27 +0000

    Isolated get_average and get_triple_product from each file and added them to their own file for neater code. Also added __pycache__ to gitignore

--- week1(varying_Pnbi).py
+++ week1(varying_Pnbi).py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+from EM1PythonFunctions import get_average, get_triple_product
+
 # from matplotlib.widgets import CheckButtons
 
 variables = ["ni0", "tite", "taue", "betap"]
@@ -29,36 +31,6 @@ files_paths = [
 ]
 
 
-def get_average(file_path, start, end, variables):
-    full_dataset = scipy.io.loadmat(file_path)
-    results = []
-    for variable in variables:
-        a = full_dataset["post"][chosen_subsection][0][0][variable][0][0]
-        a = [float(x[0]) for x in a]
-        avg = np.mean(a[start:end])
-        std = np.std(a[start:end])
-        results.append([variable, avg, std])
-    return results
-
-
-
-def get_triple_product(file_path, start, end):
-    full_dataset = scipy.io.loadmat(file_path)
-    triple_product_variables = ["ni0", "tite", "taue"]
-    results = []
-    for variable in triple_product_variables:
-        a = full_dataset["post"]["zerod"][0][0][variable][0][0]
-        a = [float(x[0]) for x in a]
-        avg = np.mean(a[start:end])
-        std = np.std(a[start:end])
-        results.append([variable, avg, std])
-    triple_product_avg = results[0][1] * results[1][1] * results[2][1]
-    triple_product_std = triple_product_avg * np.sqrt(
-        (results[0][2] / results[0][1]) ** 2
-        + (results[1][2] / results[1][1]) ** 2
-        + (results[2][2] / results[2][1]) ** 2
-    )
-    return ["triple_product", triple_product_avg, triple_product_std]
     
 
 
--- week3(full_ramp).py
+++ week3(full_ramp).py
@@ -2,6 +2,7 @@ import scipy.io
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from EM1PythonFunctions import get_average, get_triple_product
 
 chosen_subsection = "zerod"
 variables = ["taue", "betan", "modeh", "qeff"]
@@ -24,34 +25,7 @@ files_paths = [
 
 
 
-def get_triple_product(file_path, start, end):
-    full_dataset = scipy.io.loadmat(file_path)
-    triple_product_variables = ["ni0", "tite", "taue"]
-    results = []
-    for variable in triple_product_variables:
-        a = full_dataset["post"]["zerod"][0][0][variable][0][0]
-        a = [float(x[0]) for x in a]
-        avg = np.mean(a[start:end])
-        std = np.std(a[start:end])
-        results.append([variable, avg, std])
-    triple_product_avg = results[0][1] * results[1][1] * results[2][1]
-    triple_product_std = triple_product_avg * np.sqrt(
-        (results[0][2] / results[0][1]) ** 2
-        + (results[1][2] / results[1][1]) ** 2
-        + (results[2][2] / results[2][1]) ** 2
-    )
-    return ["triple_product", triple_product_avg, triple_product_std]
 
-def get_average(file_path, start, end, variables):
-    full_dataset = scipy.io.loadmat(file_path)
-    results = []
-    for variable in variables:
-        a = full_dataset["post"][chosen_subsection][0][0][variable][0][0]
-        a = [float(x[0]) for x in a]
-        avg = np.mean(a[start:end])
-        std = np.std(a[start:end])
-        results.append([variable, avg, std])
-    return results
 
 fig, axs = plt.subplots(1, len(variables) + 1, figsize=(15, 5))
 plt.rcParams["figure.dpi"] = 150  # Sets the resolution of the figure (dots per inch)
