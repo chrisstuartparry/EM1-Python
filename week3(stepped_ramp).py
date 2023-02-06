8c42013872a6088bff5c0a004537a94f006e0ef4
chronque <csg524@york.ac.uk>
2023-02-06 16:53:32 +0000

    added latex symbol dictionary

--- week3(stepped_ramp).py
+++ week3(stepped_ramp).py
@@ -4,17 +4,58 @@ import matplotlib.pyplot as plt
 import os
 from EM1PythonFunctions import get_average, get_triple_product, get_variable
 
+plt.rcParams["text.usetex"] = True
+plt.rcParams["text.latex.preamble"] = "\n".join(
+    [
+        r"\usepackage{siunitx}",
+    ]
+)
+
 chosen_subsection = "zerod"
 variables = ["taue", "betan", "modeh", "qeff"]
 # variables = ["taue", "q0", "q95", "qeff"]
-units = {
+variable_symbols = {
+    "taue": r"$\tau_E$",
+    "betan": r"$\beta_{\mathrm{normalised}}$",
+    "modeh": r"H-Mode",
+    "qeff": r"$q_{\mathrm{effective}}$",
+    "q0": r"$q_0$",
+    "q95": r"$q_{95}$",
+    "temps": r"$t$",
+    "pnbi": r"$PNBI_{\mathrm{input}}$",
+    "frnbi": r"$PNBI_{\mathrm{frac absorbed in plasma}}$",
+    "ip:": r"$I_{\mathrm{plasma}}$",
+    "betap": r"$\beta_{\mathrm{poloidal}}$",
+    "nbar": r"$\bar{n}$",
+    "ne0": r"$n_{e(0)}$",
+    "ni0": r"$n_{i(0)}$",
+    "pfus": r"$P_{\mathrm{fusion}}$",
+    "sext": r"$S_{\mathrm{external}}$",
+    "vp": r"$V_{\mathrm{plasma}}$",
+    "W": r"$E_{\mathrm{total in plasma}}$",
+}
+
+variable_units = {
     "taue": r"\unit{\second}",
     "betan": "",
     "modeh": "",
     "qeff": "",
+    "q0": "",
+    "q95": "",
     "temps": r"\unit{\second}",
     "pnbi": r"\unit{\watt}",
+    "frnbi": "",
+    "ip:": r"$\unit{ampere}$",
+    "betap": "",
+    "nbar": "",
+    "ne0": "",
+    "ni0": "",
+    "pfus": r"\unit{\watt}",
+    "sext": r"\unit{\metre\squared}",
+    "vp": r"\unit{\metre\cubed}",
+    "W": r"\unit{\joule}",
 }
+
 start = 50
 end = 100
 save_graph = False
@@ -43,12 +84,6 @@ nrows = len(files_paths)
 ncols = len(variables)
 fig, axs = plt.subplots(nrows, ncols, figsize=(15, 5 * nrows), constrained_layout=True)
 # plt.rcParams["figure.dpi"] = 150  # Sets the resolution of the figure (dots per inch)
-plt.rcParams["text.usetex"] = True
-plt.rcParams["text.latex.preamble"] = "\n".join(
-    [
-        r"\usepackage{siunitx}",
-    ]
-)
 fig.suptitle(
     "Plots of the chosen variables against Time (s)",
     fontsize=16,
@@ -59,8 +94,16 @@ def plot_variable(files_paths, first_file_values, last_file_values, variables, a
     for i, file_path in enumerate(files_paths):
         for j, variable in enumerate(variables):
             ax = axs[i, j]
-            if i == 0:
-                ax.set_title(f"{variable}, {units[variable]} against Time (s)")
+            if variable_units[variable] != "":
+                if i == 0:
+                    ax.set_title(
+                        f'{variable_symbols[variable]} ({variable_units[variable]}) against Time ({variable_units["temps"]})'
+                    )
+            else:
+                if i == 0:
+                    ax.set_title(
+                        f'{variable_symbols[variable]} against Time ({variable_units["temps"]})'
+                    )
             time_results = get_variable(file_path, ["temps"])
             times = time_results[0][1]
             results = get_variable(file_path, variables)
