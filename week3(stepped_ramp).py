dd6520cb16ce5408f3927eeae1bd826512087865
chronque <csg524@york.ac.uk>
2023-02-06 13:07:09 +0000

    made stepped_ramp work

--- week3(stepped_ramp).py
+++ week3(stepped_ramp).py
@@ -2,6 +2,7 @@ import scipy.io
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from EM1PythonFunctions import get_average, get_triple_product, get_variable
 
 chosen_subsection = "zerod"
 variables = ["taue", "betan", "modeh", "qeff"]
@@ -12,48 +13,27 @@ save_graph = False
 if save_graph:
     fig_file = input("Enter the name of the file to save the graph to: ")
 
-base_path = "EM1 Data/8th Run Data (fast mode)"
-file_name_template = "2023-02-03 NBI Ramping 0 to {last_pnbi_value}MW.mat"
-last_pnbi_values = np.array(range(0,11,2), dtype=float)
+base_path = "EM1 Data/7th Run Data (fast mode)"
+file_name_template = (
+    "2023-02-03 NBI Ramping {first_pnbi_value} to {last_pnbi_value}MW.mat"
+)
+first_file_values = np.array(range(0, 9, 2))
+last_file_values = np.array(range(2, 11, 2))
 
 files_paths = [
-    os.path.join(base_path, file_name_template.format(last_pnbi_value
-=value))
-    for value in last_pnbi_values
+    os.path.join(
+        base_path,
+        file_name_template.format(
+            first_pnbi_value=first_value, last_pnbi_value=last_value
+        ),
+    )
+    for first_value, last_value in zip(first_file_values, last_file_values)
 ]
 
 
-
-def get_triple_product(file_path, start, end):
-    full_dataset = scipy.io.loadmat(file_path)
-    triple_product_variables = ["ni0", "tite", "taue"]
-    results = []
-    for variable in triple_product_variables:
-        a = full_dataset["post"]["zerod"][0][0][variable][0][0]
-        a = [float(x[0]) for x in a]
-        avg = np.mean(a[start:end])
-        std = np.std(a[start:end])
-        results.append([variable, avg, std])
-    triple_product_avg = results[0][1] * results[1][1] * results[2][1]
-    triple_product_std = triple_product_avg * np.sqrt(
-        (results[0][2] / results[0][1]) ** 2
-        + (results[1][2] / results[1][1]) ** 2
-        + (results[2][2] / results[2][1]) ** 2
-    )
-    return ["triple_product", triple_product_avg, triple_product_std]
-
-def get_average(file_path, start, end, variables):
-    full_dataset = scipy.io.loadmat(file_path)
-    results = []
-    for variable in variables:
-        a = full_dataset["post"][chosen_subsection][0][0][variable][0][0]
-        a = [float(x[0]) for x in a]
-        avg = np.mean(a[start:end])
-        std = np.std(a[start:end])
-        results.append([variable, avg, std])
-    return results
-
-fig, axs = plt.subplots(1, len(variables) + 1, figsize=(15, 5))
+nrows = len(files_paths)
+ncols = len(variables)
+fig, axs = plt.subplots(nrows, ncols, figsize=(15, 5 * nrows), constrained_layout=True)
 plt.rcParams["figure.dpi"] = 150  # Sets the resolution of the figure (dots per inch)
 plt.rcParams["text.usetex"] = True
 plt.rcParams["text.latex.preamble"] = "\n".join(
@@ -62,28 +42,29 @@ plt.rcParams["text.latex.preamble"] = "\n".join(
     ]
 )
 fig.suptitle(
-    "Plots of the average values of the chosen variables against NBI power",
+    "Plots of the chosen variables against Time (s)",
     fontsize=16,
 )
-axs[0].set_title("Triple Product")
-axs[0].set_xlabel("NBI Power (MW)")
-axs[0].set_ylabel("nTtaue")
-for file_path, power in zip(files_paths, last_pnbi_values):
-    results = get_triple_product(file_path, start, end)
-    triple_product, avg, std = results
-    axs[0].errorbar(power, avg, yerr=std, fmt=".", color="black", elinewidth=0.5)
-for i, variable in enumerate(variables):
-    axs[i+1].set_title(f"{variable}")
-    axs[i+1].set_xlabel("NBI Power (MW)")
-    axs[i+1].set_ylabel(variable)
-    for file_path, power in zip(files_paths, last_pnbi_values):
-        # print(f"Getting data for {variable} at {power} MW")
-        results = get_average(file_path, start, end, variables)
-        variable, avg, std = results[i]
-        # print("Average: ", avg, "Standard Deviation: ", std, "Variable: ", variable)
-        # print(f"Plotting {variable} at {power} MW")
-        axs[i+1].errorbar(power, avg, yerr=std, fmt=".", color="black", elinewidth=0.5)
-fig.tight_layout()
+
+
+def plot_variable(files_paths, first_file_values, last_file_values, variables, axs):
+    for i, file_path in enumerate(files_paths):
+        for j, variable in enumerate(variables):
+            ax = axs[i, j]
+            if i == 0:
+                ax.set_title(f"{variable}")
+            time_results = get_variable(file_path, ["temps"])
+            times = time_results[0][1]
+            results = get_variable(file_path, variables)
+            variable, ydata = results[j]
+            ax.plot(times, ydata, ".", color="black")
+            if i == nrows - 1:
+                ax.set_xlabel("Time (s)")
+            if j == 0:
+                ax.set_ylabel(f"{first_file_values[i]}MW to {last_file_values[i]}MW")
+
+
+plot_variable(files_paths, first_file_values, last_file_values, variables, axs)
 if save_graph:
     plt.savefig(fig_file, dpi=500)
 plt.show()
