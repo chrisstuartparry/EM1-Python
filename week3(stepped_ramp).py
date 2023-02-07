69a519c1acc7565af385a921e4042376e57ea3a1
chronque <csg524@york.ac.uk>
2023-02-07 13:50:46 +0000

    Got plots working with y axes and row headers

--- week3(stepped_ramp).py
+++ week3(stepped_ramp).py
@@ -2,7 +2,12 @@ import scipy.io
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-from EM1PythonFunctions import get_average, get_triple_product, get_variable
+from EM1PythonFunctions import (
+    get_average,
+    get_triple_product,
+    get_variable,
+    add_headers,
+)
 from EM1PythonDictionaries import variable_symbols, variable_units, variable_meanings
 
 plt.rcParams["text.usetex"] = True
@@ -15,7 +20,6 @@ plt.rcParams["text.latex.preamble"] = "\n".join(
 chosen_subsection = "zerod"
 # variables = ["taue", "betan", "modeh", "qeff"]
 variables = ["taue", "q0", "q95", "qeff"]
-
 
 start = 50
 end = 100
@@ -46,12 +50,20 @@ ncols = len(variables)
 fig, axs = plt.subplots(nrows, ncols, figsize=(15, 5 * nrows), constrained_layout=True)
 # plt.rcParams["figure.dpi"] = 150  # Sets the resolution of the figure (dots per inch)
 fig.suptitle(
-    "Plots of the chosen variables against Time (s)",
+    f'Plots of the chosen variables against {variable_symbols["temps"]}{variable_units["temps"]}',
     fontsize=16,
 )
 
 
-def plot_variable(files_paths, first_file_values, last_file_values, variables, axs):
+def plot_variable(
+    files_paths,
+    first_file_values,
+    last_file_values,
+    variables,
+    axs,
+    row_header_yesno=True,
+):
+    row_headers = []
     for i, file_path in enumerate(files_paths):
         for j, variable in enumerate(variables):
             ax = axs[i, j]
@@ -73,9 +85,21 @@ def plot_variable(files_paths, first_file_values, last_file_values, variables, a
             ax.plot(times, ydata, ".", color="black")
             if i == nrows - 1:
                 ax.set_xlabel(f'{variable_symbols["temps"]} {variable_units["temps"]}')
+            if row_header_yesno:
+                row_headers.append(
+                    f"{first_file_values[i]}MW to {last_file_values[i]}MW"
+                )
+    return row_headers
 
 
 plot_variable(files_paths, first_file_values, last_file_values, variables, axs)
+row_headers = plot_variable(
+    files_paths, first_file_values, last_file_values, variables, axs
+)
+print(row_headers)
+add_headers(fig, row_headers=row_headers)
+
+
 if save_graph:
     plt.savefig(fig_file, dpi=500)
 plt.show()
