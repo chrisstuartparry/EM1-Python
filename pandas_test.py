040c7b1b92e496f630f4e5f17f35944bdbe9e943
chronque <csg524@york.ac.uk>
2023-03-25 17:18:44 +0000

    Converted nTtau addition to a function

--- pandas_test.py
+++ pandas_test.py
@@ -21,40 +21,44 @@ def mat_to_DataFrame(file_path, chosen_structure="post", chosen_substructure="ze
     substructure = structure[chosen_substructure][0, 0]
 
     # Create a dictionary of the field names and corresponding data, one for arrays and one for scalars
+
     array_data_dict = {}
     scalar_data_dict = {}
     for field_name in substructure.dtype.names:
         field_data = substructure[field_name][0, 0]
-        if field_data.size == 1:  # if scalar
+        if field_data.size == 1:
             scalar_data_dict[field_name] = field_data[0]
-        else:  # if array
+        else:
             array_data_dict[field_name] = field_data.squeeze()
 
-    # Convert the array dictionary to a pandas DataFrame
+    # Convert the array dictionary to a pandas DataFrame, and the scalar data to a pandas Series
     df = pd.DataFrame(array_data_dict)
-
-    # Convert the scalar data to a pandas Series
     series = pd.Series(scalar_data_dict)
 
     return df
 
 
+def load_data(file_path):
+    default_index_dataframe = mat_to_DataFrame(file_path)
+    temps_index_dataframe = default_index_dataframe.set_index("temps")
+
+    for column in temps_index_dataframe.columns:
+        if column not in variables_list:
+            temps_index_dataframe.drop(column, axis=1, inplace=True)
+
+    temps_index_dataframe["tim"] = (
+        temps_index_dataframe["tite"] * temps_index_dataframe["tem"]
+    )
+
+    temps_index_dataframe["nTtau"] = (
+        temps_index_dataframe["nim"]
+        * temps_index_dataframe["tim"]
+        * temps_index_dataframe["taue"]
+    )
+
+    return temps_index_dataframe
+
+
 file_path = "EM1 Data/4th Run Data (fast mode)/2023-01-27 NBI Power 2MW B0 0.1T.mat"
-
-default_index_dataframe = mat_to_DataFrame(file_path)
-temps_index_dataframe = default_index_dataframe.set_index("temps")
-# print(temps_index_dataframe)
-
-for column in temps_index_dataframe.columns:
-    if column not in variables_list:
-        temps_index_dataframe.drop(column, axis=1, inplace=True)
-
-temps_index_dataframe["tim"] = (
-    temps_index_dataframe["tite"] * temps_index_dataframe["tem"]
-)
-
-temps_index_dataframe["nTtau"] = (
-    temps_index_dataframe["nim"]
-    * temps_index_dataframe["tim"]
-    * temps_index_dataframe["taue"]
-)
+dataframe = load_data(file_path)
+print(dataframe)
