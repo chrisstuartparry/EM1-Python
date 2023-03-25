595fd8eed9169660834f0eca6dfd35a8b301417f
chronque <csg524@york.ac.uk>
2023-03-25 23:51:40 +0000

    Added class and subsequent instances of that class to allow one script to open all of the required data files for the entire experiment, excluding the stepped ramp

--- pandas_test.py
+++ pandas_test.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import tkinter as tk
+import tkinter.filedialog as fd
 from EM1PythonDictionaries import (
     variable_meanings,
     variable_symbols,
@@ -38,7 +40,7 @@ def mat_to_DataFrame(file_path, chosen_structure="post", chosen_substructure="ze
     return df
 
 
-def load_data(file_path):
+def load_data_into_dataframe(file_path):
     default_index_dataframe = mat_to_DataFrame(file_path)
     temps_index_dataframe = default_index_dataframe.set_index("temps")
 
@@ -59,6 +61,118 @@ def load_data(file_path):
     return temps_index_dataframe
 
 
-file_path = "EM1 Data/4th Run Data (fast mode)/2023-01-27 NBI Power 2MW B0 0.1T.mat"
-dataframe = load_data(file_path)
-print(dataframe)
+# file_path = "EM1 Data/4th Run Data (fast mode)/2023-01-27 NBI Power 2MW B0 0.1T.mat"
+
+
+class FilePathListGenerator:
+    def __init__(self, base_path, file_name_template, file_values):
+        self.base_path = base_path
+        self.file_name_template = file_name_template
+        self.file_values = file_values
+
+    def generate_file_paths(self):
+        return [
+            os.path.join(self.base_path, self.file_name_template.format(**value))
+            for value in self.file_values
+        ]
+
+    def get_file_paths(self, user_decides=False):
+        if user_decides:
+            root = tk.Tk()
+            root.withdraw()
+            file_paths = fd.askopenfilenames(parent=root, title="Choose file(s)")
+            root.destroy()
+        else:
+            return self.generate_file_paths()
+
+
+pnbi_file_path_list_generator = FilePathListGenerator(
+    base_path="EM1 Data/3rd Run Data (fast mode)",
+    file_name_template="2023-01-25 NBI Power {NBI_power}MW.mat",
+    file_values=[
+        {"NBI_power": value}
+        for value in [
+            0,
+            0.25,
+            0.5,
+            0.75,
+            1,
+            1.1,
+            1.2,
+            1.3,
+            1.4,
+            1.5,
+            1.6,
+            1.7,
+            1.8,
+            1.9,
+        ]
+        + list(range(2, 41, 2))
+    ],
+)
+pnbi_file_paths = pnbi_file_path_list_generator.get_file_paths(user_decides=False)
+pnbi_dataframes = [load_data_into_dataframe(file_path) for file_path in pnbi_file_paths]
+pnbi_dataframes_dictionary = {
+    os.path.basename(file_path): pnbi_dataframe
+    for file_path, pnbi_dataframe in zip(pnbi_file_paths, pnbi_dataframes)
+}
+
+B0_file_path_list_generator = FilePathListGenerator(
+    base_path="EM1 Data/4th Run Data (fast mode)",
+    file_name_template="2023-01-27 NBI Power 2MW B0 {B0_value}T.mat",
+    file_values=[
+        {"B0_value": value} for value in (np.array(range(1, 41, 1), dtype=float) / 10)
+    ],
+)
+B0_file_paths = B0_file_path_list_generator.get_file_paths(user_decides=False)
+B0_dataframes = [load_data_into_dataframe(file_path) for file_path in B0_file_paths]
+B0_dataframes_dictionary = {
+    os.path.basename(file_path): B0_dataframe
+    for file_path, B0_dataframe in zip(B0_file_paths, B0_dataframes)
+}
+
+Ip_file_path_list_generator = FilePathListGenerator(
+    base_path="EM1 Data/5th Run Data (fast mode)/",
+    file_name_template="2023-01-31 NBI Power 2MW Ip {Ip_value}MA.mat",
+    file_values=[
+        {"Ip_value": value} for value in (np.array(range(1, 51, 1), dtype=float) / 10)
+    ],
+)
+Ip_file_paths = Ip_file_path_list_generator.get_file_paths(user_decides=False)
+Ip_dataframes = [load_data_into_dataframe(file_path) for file_path in Ip_file_paths]
+Ip_dataframes_dictionary = {
+    os.path.basename(file_path): Ip_dataframe
+    for file_path, Ip_dataframe in zip(Ip_file_paths, Ip_dataframes)
+}
+
+nbar_file_path_list_generator = FilePathListGenerator(
+    base_path="EM1 Data/6th Run Data (fast mode)",
+    file_name_template="2023-01-31 NBI Power 2MW nbar {nbar_value}.mat",
+    file_values=[
+        {"nbar_value": value} for value in (np.array(range(1, 41, 1), dtype=float) / 10)
+    ],
+)
+nbar_file_paths = nbar_file_path_list_generator.get_file_paths(user_decides=False)
+nbar_dataframes = [load_data_into_dataframe(file_path) for file_path in nbar_file_paths]
+nbar_dataframes_dictionary = {
+    os.path.basename(file_path): nbar_dataframe
+    for file_path, nbar_dataframe in zip(nbar_file_paths, nbar_dataframes)
+}
+
+full_ramp_file_path_list_generator = FilePathListGenerator(
+    base_path="EM1 Data/8th Run Data (fast mode)",
+    file_name_template=("2023-02-03 NBI Ramping 0 to {last_pnbi_value}MW.mat"),
+    file_values=[{"last_pnbi_value": value} for value in np.array(range(2, 11, 2))],
+)
+full_ramp_file_paths = full_ramp_file_path_list_generator.get_file_paths(
+    user_decides=False
+)
+full_ramp_dataframes = [
+    load_data_into_dataframe(file_path) for file_path in full_ramp_file_paths
+]
+full_ramp_dataframes_dictionary = {
+    os.path.basename(file_path): full_ramp_dataframe
+    for file_path, full_ramp_dataframe in zip(
+        full_ramp_file_paths, full_ramp_dataframes
+    )
+}
