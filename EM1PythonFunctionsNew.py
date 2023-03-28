7cef674cf990a16bb9f47848f569fbed499852b4
chronque <csg524@york.ac.uk>
2023-03-28 11:32:02 +0100

    Radio buttons nearly working

--- EM1PythonFunctionsNew.py
+++ EM1PythonFunctionsNew.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 from varname import nameof
-from matplotlib.widgets import Slider
+from matplotlib.widgets import RadioButtons
 from EM1PythonDictionaries import (
     variable_meanings,
     variable_symbols,
@@ -59,3 +59,64 @@ def get_averages_and_stds(dataframes_list, variables, start=50, end=100):
         averages_and_stds[variable + "_average"] = variable_averages
         averages_and_stds[variable + "_std"] = variable_stds
     return averages_and_stds
+
+
+def plot_interactive(file_path_list_generators, dataframes_lists, variables):
+    def update_plot(label):
+        for file_path_list_generator, dataframes_list, axs_group in zip(
+            file_path_list_generators, dataframes_lists, axs_groups
+        ):
+            visible = file_path_list_generator.base_path == label
+            for axs in axs_group:
+                for ax in axs:
+                    ax.set_visible(visible)
+        plt.draw()
+
+    # Create the figures and axes
+    axs_groups = [
+        generate_fig_and_axs(variables, nameof(file_path_list_generator.file_values[0]))
+        for file_path_list_generator in file_path_list_generators
+    ]
+    # Plot the data
+    for file_path_list_generator, dataframes_list, axs_group in zip(
+        file_path_list_generators, dataframes_lists, axs_groups
+    ):
+        x_parameter = nameof(file_path_list_generator.file_values[0])
+        averages_and_stds_dict = get_averages_and_stds(dataframes_list, variables)
+        for i, variable in enumerate(variables):
+            axs = axs_group[1][i]
+            axs.set_title(
+                f"{variable_meanings[variable]} vs. {parameter_symbols[x_parameter]}",
+                fontsize=10,
+            )
+            axs.set_xlabel(
+                f"{parameter_symbols[x_parameter]} ({parameter_units[x_parameter]})"
+            )
+            axs.set_ylabel(f"{variable_symbols[variable]} ({variable_units[variable]})")
+            file_values = file_path_list_generator.file_values
+            key = list(file_values[0].keys())[0]
+            x = [item[key] for item in file_values]
+            y = averages_and_stds_dict[variable + "_average"]
+            yerr = averages_and_stds_dict[variable + "_std"]
+            axs.errorbar(x, y, yerr=yerr, fmt=".", color="black", elinewidth=0.5)
+            axs.set_visible(False)
+
+    # Set the first set of axes as visible
+    for ax in axs_groups[0][1]:
+        ax.set_visible(True)
+
+    # Create the radio button widget
+    ax_radio = plt.axes([0.05, 0.4, 0.1, 0.15])
+    radio = RadioButtons(
+        ax_radio,
+        [
+            nameof(file_path_list_generator.file_values[0])
+            for file_path_list_generator in file_path_list_generators
+        ],
+    )
+
+    # Connect the radio buttons to the update_plot function
+    radio.on_clicked(update_plot)
+
+    # Display the plot with the interactive widget
+    plt.show()
--- pandas_test.py
+++ pandas_test.py
@@ -5,9 +5,13 @@ import matplotlib.pyplot as plt
 import os
 import tkinter as tk
 import tkinter.filedialog as fd
-from matplotlib.widgets import Slider
+from matplotlib.widgets import RadioButtons
 from varname import nameof
-from EM1PythonFunctionsNew import generate_fig_and_axs, get_averages_and_stds
+from EM1PythonFunctionsNew import (
+    generate_fig_and_axs,
+    get_averages_and_stds,
+    # plot_interactive,
+)
 from EM1PythonDictionaries import (
     variable_meanings,
     variable_symbols,
@@ -72,6 +76,66 @@ def load_data_into_dataframe(file_path):
     return temps_index_dataframe
 
 
+def plot_interactive(
+    file_path_list_generators, dataframes_lists, x_parameters, variables
+):
+    def update_plot(label):
+        for file_path_list_generator, dataframes_list, axs_group in zip(
+            file_path_list_generators, dataframes_lists, axs_groups
+        ):
+            visible = file_path_list_generator.base_path == label
+            for axs in axs_group:
+                for ax in axs:
+                    ax.set_visible(visible)
+        plt.draw()
+
+    # Create the figures and axes
+    for file_path_list_generator, dataframes in zip(
+        file_path_list_generators, dataframes_lists
+    ):
+        file_values_name = list(file_path_list_generator.file_values[0].keys())[0]
+        print(file_values_name)
+        axs_groups = [generate_fig_and_axs(variables, file_values_name)]
+    # Plot the data
+    for file_path_list_generator, dataframes_list, axs_group in zip(
+        file_path_list_generators, dataframes_lists, axs_groups
+    ):
+        x_parameter = list(file_path_list_generator.file_values[0].keys())[0]
+        print("x_parameter: ", x_parameter)
+        averages_and_stds_dict = get_averages_and_stds(dataframes_list, variables)
+        for i, variable in enumerate(variables):
+            axs = axs_group[1][i]
+            axs.set_title(
+                f"{variable_meanings[variable]} vs. {parameter_symbols[x_parameter]}",
+                fontsize=10,
+            )
+            axs.set_xlabel(
+                f"{parameter_symbols[x_parameter]} ({parameter_units[x_parameter]})"
+            )
+            axs.set_ylabel(f"{variable_symbols[variable]} ({variable_units[variable]})")
+            file_values = file_path_list_generator.file_values
+            key = list(file_values[0].keys())[0]
+            x = [item[key] for item in file_values]
+            y = averages_and_stds_dict[variable + "_average"]
+            yerr = averages_and_stds_dict[variable + "_std"]
+            axs.errorbar(x, y, yerr=yerr, fmt=".", color="black", elinewidth=0.5)
+            axs.set_visible(False)
+
+    # Set the first set of axes as visible
+    for ax in axs_groups[0][1]:
+        ax.set_visible(True)
+
+    # Create the radio button widget
+    ax_radio = plt.axes([0.05, 0.4, 0.1, 0.15])
+    radio = RadioButtons(
+        ax_radio,
+        [
+            file_path_list_generator.file_values
+            for file_path_list_generator in file_path_list_generators
+        ],
+    )
+
+
 class FilePathListGenerator:
     def __init__(self, base_path, file_name_template, file_values):
         self.base_path = base_path
@@ -121,9 +185,9 @@ class FilePathListGenerator:
 
 pnbi_file_path_list_generator = FilePathListGenerator(
     base_path="EM1 Data/3rd Run Data (fast mode)",
-    file_name_template="2023-01-25 NBI Power {NBI_power}MW.mat",
+    file_name_template="2023-01-25 NBI Power {NBI}MW.mat",
     file_values=[
-        {"NBI_power": value}
+        {"NBI": value}
         for value in [
             0,
             0.25,
@@ -152,9 +216,9 @@ pnbi_dataframes = [load_data_into_dataframe(file_path) for file_path in pnbi_fil
 
 B0_file_path_list_generator = FilePathListGenerator(
     base_path="EM1 Data/4th Run Data (fast mode)",
-    file_name_template="2023-01-27 NBI Power 2MW B0 {B0_value}T.mat",
+    file_name_template="2023-01-27 NBI Power 2MW B0 {b0}T.mat",
     file_values=[
-        {"B0_value": value} for value in (np.array(range(1, 41, 1), dtype=float) / 10)
+        {"b0": value} for value in (np.array(range(1, 41, 1), dtype=float) / 10)
     ],
 )
 B0_file_paths = B0_file_path_list_generator.get_file_paths(user_decides=False)
@@ -166,9 +230,9 @@ B0_dataframes = [load_data_into_dataframe(file_path) for file_path in B0_file_pa
 
 Ip_file_path_list_generator = FilePathListGenerator(
     base_path="EM1 Data/5th Run Data (fast mode)/",
-    file_name_template="2023-01-31 NBI Power 2MW Ip {Ip_value}MA.mat",
+    file_name_template="2023-01-31 NBI Power 2MW Ip {Ip}MA.mat",
     file_values=[
-        {"Ip_value": value} for value in (np.array(range(1, 51, 1), dtype=float) / 10)
+        {"Ip": value} for value in (np.array(range(1, 51, 1), dtype=float) / 10)
     ],
 )
 Ip_file_paths = Ip_file_path_list_generator.get_file_paths(user_decides=False)
@@ -180,9 +244,9 @@ Ip_dataframes = [load_data_into_dataframe(file_path) for file_path in Ip_file_pa
 
 nbar_file_path_list_generator = FilePathListGenerator(
     base_path="EM1 Data/6th Run Data (fast mode)",
-    file_name_template="2023-01-31 NBI Power 2MW nbar {nbar_value}.mat",
+    file_name_template="2023-01-31 NBI Power 2MW nbar {Nbar}.mat",
     file_values=[
-        {"nbar_value": value} for value in (np.array(range(1, 41, 1), dtype=float) / 10)
+        {"Nbar": value} for value in (np.array(range(1, 41, 1), dtype=float) / 10)
     ],
 )
 nbar_file_paths = nbar_file_path_list_generator.get_file_paths(user_decides=False)
@@ -212,7 +276,32 @@ full_ramp_dataframes = [
 
 variables = ["nTtau", "ni0", "taue", "tite", "tem"]
 
-pnbi_file_path_list_generator.plot_averages("NBI", pnbi_dataframes, variables)
-B0_file_path_list_generator.plot_averages("b0", B0_dataframes, variables)
-Ip_file_path_list_generator.plot_averages("Ip", Ip_dataframes, variables)
-nbar_file_path_list_generator.plot_averages("Nbar", nbar_dataframes, variables)
+# pnbi_file_path_list_generator.plot_averages("NBI", pnbi_dataframes, variables)
+# B0_file_path_list_generator.plot_averages("b0", B0_dataframes, variables)
+# Ip_file_path_list_generator.plot_averages("Ip", Ip_dataframes, variables)
+# nbar_file_path_list_generator.plot_averages("Nbar", nbar_dataframes, variables)
+
+# plot_interactive(pnbi_file_path_list_generator, pnbi_dataframes, variables)
+# plot_interactive(B0_file_path_list_generator, B0_dataframes, variables)
+# plot_interactive(Ip_file_path_list_generator, Ip_dataframes, variables)
+# plot_interactive(nbar_file_path_list_generator, nbar_dataframes, variables)
+
+file_path_list_generators_to_plot = [
+    pnbi_file_path_list_generator,
+    B0_file_path_list_generator,
+    Ip_file_path_list_generator,
+    nbar_file_path_list_generator,
+]
+
+dataframes_lists_to_plot = [
+    pnbi_dataframes,
+    B0_dataframes,
+    Ip_dataframes,
+    nbar_dataframes,
+]
+
+x_parameters = ["pnbi", "b0", "Ip", "Nbar"]
+
+plot_interactive(
+    file_path_list_generators_to_plot, dataframes_lists_to_plot, x_parameters, variables
+)
