79e48028afafdde17a8bb01d44fb764027b14742
chronque <csg524@york.ac.uk>
2023-04-06 23:25:20 +0100

    making progress

--- EM1PythonClasses.py
+++ EM1PythonClasses.py
@@ -43,6 +43,8 @@ class DataProcessor:
         self.file_name_template = file_name_template
         self.glob_file_name_template = file_name_template.format("*")
         self.primary_x_parameter = primary_x_parameter
+        self.x_parameter_list: list[dict[str, float]] = self.generate_x_parameter_list()
+        self.list_of_files_via_glob: list[str] = self.get_files_list()
 
     def get_files_list(self) -> list[str]:
         glob_file_path = os.path.join(self.base_path, self.glob_file_name_template)
@@ -51,12 +53,11 @@ class DataProcessor:
 
     def generate_x_parameter_list(self) -> list[dict[str, float]]:
         files_list: list[str] = self.get_files_list()
-        regex_pattern = self.file_name_template.format(r"(\d+)")
+        regex_pattern = self.file_name_template.format(r"(\d+(\.\d+)?)")
 
-        x_parameter_list: list[dict[str, float]] = [
+        x_parameter_list = [
             {f"{self.primary_x_parameter}": float(match.group(1))}
             for file in files_list
             if (match := re.search(regex_pattern, file))
         ]
-
         return x_parameter_list
--- main_new.py
+++ main_new.py
@@ -5,3 +5,11 @@ from datetime import datetime
 from pandas import DataFrame
 from EM1PythonFunctions import plot_all, load_data_into_dataframe
 from EM1PythonClasses import DataProcessor
+
+pnbi_class = DataProcessor(
+    base_path="EM1 Data/3rd Run Data (fast mode)",
+    file_name_template="NBI Power {}MW.mat",
+    primary_x_parameter="pnbi",
+)
+print(pnbi_class.x_parameter_list)
+# print(pnbi_class.list_of_files_via_glob)
